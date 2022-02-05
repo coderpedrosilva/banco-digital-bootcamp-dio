d746dfdd66e23bddd9e28c409816948909b726b6
Pedro Silva <prezadopedro@hotmail.com>
2022-02-05 17:48:50 -0300

    Implemented inheritance

--- src/entities/ContaCorrente.java
+++ src/entities/ContaCorrente.java
@@ -1,33 +1,5 @@
 package entities;
 
-public class ContaCorrente {
-
-	private int agencia;
-	private int numero;
-	private int saldo;
-
-	public void sacar() {
-
-	}
-
-	public void depositar() {
-
-	}
-
-	public void transferir() {
-
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public int getSaldo() {
-		return saldo;
-	}
+public class ContaCorrente extends Conta {
 
 }
--- src/entities/ContaPoupanca.java
+++ src/entities/ContaPoupanca.java
@@ -1,33 +1,5 @@
 package entities;
 
-public class ContaPoupanca {
-
-	private int agencia;
-	private int numero;
-	private int saldo;
-
-	public void sacar() {
-
-	}
-
-	public void depositar() {
-
-	}
-
-	public void transferir() {
-
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public int getSaldo() {
-		return saldo;
-	}
+public class ContaPoupanca extends Conta {
 
 }
