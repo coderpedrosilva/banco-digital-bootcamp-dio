72a188b9c9e45f36eb8e20504a73ce3caa4e5e0e
Pedro Silva <you@example.com>
2022-02-05 16:56:23 -0300

    entities creation

---
+++ src/entities/ContaCorrente.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class ContaCorrente {
+
+	public int agencia;
+	public int numero;
+	public int saldo;
+
+	public void sacar() {
+
+	}
+
+	public void depositar() {
+
+	}
+
+	public void transferir() {
+
+	}
+
+}
---
+++ src/entities/ContaPoupanca.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class ContaPoupanca {
+
+	public int agencia;
+	public int numero;
+	public int saldo;
+
+	public void sacar() {
+
+	}
+
+	public void depositar() {
+
+	}
+
+	public void transferir() {
+
+	}
+
+}
